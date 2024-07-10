372a8b7580eb1554fe34e41ffe1550202179372d
kmorale3 <89863064+kmorale3@users.noreply.github.com>
2024-07-09 19:59:58 -0600

    Update settings.py

--- changes/TinDev/settings.py
+++ changes/TinDev/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wmj1$hpl0h_pgdizetf-6%tx)9jf6zxu7c638b88)q4%8fphi9'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
